49a4a519f26e0574858e585a908114d9292455d8
mikilanjlo <mikilanjlo@yandex.by>
2019-02-22 12:07:52 +0300

    get solution

--- NullableTypeExtensions.cs
+++ NullableTypeExtensions.cs
@@ -6,8 +6,13 @@ namespace PadawansTask10
     {
         public static bool IsNull(this object variable)
         {
-            // put your code here
-            throw new NotImplementedException();
+            //Type[] types = new Type[] { Boolean, Byte, SByte, Int16, UInt16, Int32, UInt32, Int64, UInt64, Single, Double, Decimal, Char };
+            //foreach (Type type in types)
+            //    if (variable is type)
+            //        throw new ArgumentException();
+            if (variable == null)
+                return true;
+            return false;
         }
     }
 }
